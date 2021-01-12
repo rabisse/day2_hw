19288ef7872ebad97ce924f5851cd7ef42ad4e25
rabisse <rabisse@gmail.com>
2021-01-12 21:02:24 +0000

    not sure what I was saving before but hope this is right now

--- my_functions.py
+++ my_functions.py
@@ -1,10 +1,27 @@
-#defining function
-def greet():
-    print("Hello")
-greet() #prints Hello
+# #defining function
+# def greet():
+#     print("Hello")
+# greet() #prints Hello
 
 
-def greet():
-    return "Hello"
-greeting = greet() #declares variable that uses function as definition
+# def greet():
+#     return "Hello"
+# greeting = greet() #declares variable that uses function as definition
+# print(greeting)
+
+
+# def greet():
+#     print("Hello")
+# greeting = greet()
+# print(greeting)
+# #'None' get printed because nothing was 'returned' from the function, so the variable is basically empty
+
+
+def greet(name, time_of_day): #note the parameter
+  return "Good " + time_of_day + " " + name
+
+greeting = greet("Alex", "evening") #note the argument
 print(greeting)
+#=> Good evening Alex
+
+
